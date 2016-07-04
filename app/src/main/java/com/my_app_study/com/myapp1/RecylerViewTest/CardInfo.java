25e27ace4524b29c356763a2a856cb605f96b305
zhangwei <zhangwei@zhanyaa.com>
2016-07-04 11:00:57 +0800

    RecylerView上拉加载更多

---
+++ app/src/main/java/com/my_app_study/com/myapp1/RecylerViewTest/CardInfo.java
@@ -0,0 +1,25 @@
+package com.my_app_study.com.myapp1.RecylerViewTest;
+
+/**
+ * Created by Administrator on 2016/7/4.
+ */
+public class CardInfo {
+    private String content;
+    private String title;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
---
+++ app/src/main/java/com/my_app_study/com/myapp1/net/model/RsBaseModel.java
@@ -0,0 +1,8 @@
+package com.my_app_study.com.myapp1.net.model;
+
+
+public class RsBaseModel {
+    public String error_mesg;
+    public String error;
+
+}
